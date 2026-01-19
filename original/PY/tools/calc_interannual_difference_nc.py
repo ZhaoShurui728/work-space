2b6d62b99df8126a5f86a4ede3f3cb6f599de87a
ZhaoShurui728 <g2199192503@gmail.com>
2026-01-19 15:37:05 +0900

    debug: changing dim of nc

--- original/PY/tools/calc_interannual_difference_nc.py
+++ original/PY/tools/calc_interannual_difference_nc.py
@@ -1,12 +1,12 @@
 import xarray as xr
 
-nc = xr.open_dataset("./work-space/original/NC/compare.nc")
+nc = xr.open_dataset("./original/NC/compare.nc")
 
 basin = nc["basin_agri"]
 region = nc["region_agri"]
 
-basin_diff = basin.diff(dim="year")
-region_diff = region.diff(dim="year")
+basin_diff = basin.diff(dim="time")
+region_diff = region.diff(dim="time")
 
 basin_diff = basin_diff.rename("basin_agri_diff")
 region_diff = region_diff.rename("region_agri_diff")
@@ -17,4 +17,4 @@ nc_diff = xr.Dataset(
         "region_agri_diff": region_diff
     }
 )
-nc_diff.to_netcdf("./work-space/original/NC/compare_diff.nc")
+nc_diff.to_netcdf("./original/NC/compare_diff.nc")
