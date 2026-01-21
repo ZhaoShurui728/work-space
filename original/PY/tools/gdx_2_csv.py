afdbca869b4a106a2157997c5bae6b65dba7e461
ZhaoShurui728 <g2199192503@gmail.com>
2026-01-21 17:11:13 +0900

    usage change

--- original/PY/tools/gdx_2_csv.py
+++ original/PY/tools/gdx_2_csv.py
@@ -2,12 +2,12 @@ import gdxpds
 import pandas as pd
 
 # ---------- 第一次运行：读取 GDX ----------
-gdx = gdxpds.to_dataframes("./GDX/SSP2_BaU_NoCC_World.gdx")
+gdx = gdxpds.to_dataframes("./GDX/basin analysis.gdx")
 
-df = gdx["Yld2_load"].copy()
-df.columns = ["category","basin","country" ,"year","Value"]
+df = gdx["Area"].copy()
+df.columns = ["country","year","type" ,"Value"]
 
 # 保存为 CSV（这是关键）
-df.to_csv("./original/CSV/aglu/yield.csv", index=False)
+df.to_csv("./original/CSV/compare_region_basin/basin_area.csv", index=False)
 
 print("第一次处理完成，已保存csv！以后都不需要再读GDX。")
