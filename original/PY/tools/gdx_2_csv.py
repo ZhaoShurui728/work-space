65719ee82a828c58dec1a07395cf45cf7a322bad
ZhaoShurui728 <g2199192503@gmail.com>
2026-01-29 20:42:27 +0900

    Tool: Check aglu plum region mode cropland area

--- original/PY/tools/gdx_2_csv.py
+++ original/PY/tools/gdx_2_csv.py
@@ -4,10 +4,10 @@ import pandas as pd
 # ---------- 第一次运行：读取 GDX ----------
 gdx = gdxpds.to_dataframes("./GDX/agludata.gdx")
 
-df = gdx["AgLULandusedata"].copy()
-df.columns = ["scenario","basin","type","year" ,"value"]
+df = gdx["AgLULandusedata_17region"].copy()
+df.columns = ["scenario","region","type","year" ,"value"]
 
 # 保存为 CSV（这是关键）
-df.to_csv("./original/CSV/aglu/agludata.csv", index=False)
+df.to_csv("./original/CSV/aglu/agludata17.csv", index=False)
 
 print("第一次处理完成，已保存csv！以后都不需要再读GDX。")
