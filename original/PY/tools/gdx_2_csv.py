6d6a932bf6eee78cac40827bb627051551d33e4b
ZhaoShurui728 <g2199192503@gmail.com>
2026-01-29 12:43:57 +0900

    created agludata

--- original/PY/tools/gdx_2_csv.py
+++ original/PY/tools/gdx_2_csv.py
@@ -2,12 +2,12 @@ import gdxpds
 import pandas as pd
 
 # ---------- 第一次运行：读取 GDX ----------
-gdx = gdxpds.to_dataframes("./GDX/basin analysis.gdx")
+gdx = gdxpds.to_dataframes("./GDX/agludata.gdx")
 
-df = gdx["Area"].copy()
-df.columns = ["country","year","type" ,"Value"]
+df = gdx["AgLULandusedata"].copy()
+df.columns = ["scenario","basin","type","year" ,"value"]
 
 # 保存为 CSV（这是关键）
-df.to_csv("./original/CSV/compare_region_basin/basin_area.csv", index=False)
+df.to_csv("./original/CSV/aglu/agludata.csv", index=False)
 
 print("第一次处理完成，已保存csv！以后都不需要再读GDX。")
