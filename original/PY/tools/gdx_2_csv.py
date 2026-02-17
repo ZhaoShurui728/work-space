40f4a812fc6210f8f67446f1c44db38a4c9ef566
ZhaoShurui728 <g2199192503@gmail.com>
2026-02-17 13:24:44 +0900

    new csv & tool changed

--- original/PY/tools/gdx_2_csv.py
+++ original/PY/tools/gdx_2_csv.py
@@ -2,12 +2,12 @@ import gdxpds
 import pandas as pd
 
 # ---------- 第一次运行：读取 GDX ----------
-gdx = gdxpds.to_dataframes("./GDX/agludata.gdx")
+gdx = gdxpds.to_dataframes("./GDX/Mergedcomparison_region.gdx")
 
-df = gdx["AgLULandusedata_17region"].copy()
-df.columns = ["scenario","region","type","year" ,"value"]
-
+df = gdx["IAMCTemp"].copy()
+df.columns = ["scenario","region","type","*","year" ,"value"]
+df = df[df["type"] == "Emi_CO2_AFO"]
 # 保存为 CSV（这是关键）
-df.to_csv("./original/CSV/aglu/agludata17.csv", index=False)
+df.to_csv("./original/CSV/IAMC/Emi_CO2_basin.csv", index=False)
 
 print("第一次处理完成，已保存csv！以后都不需要再读GDX。")
