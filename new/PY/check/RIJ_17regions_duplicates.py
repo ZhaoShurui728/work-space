aa16b75090284380e25147515018a6eb8dfbe6ae
ZhaoShurui728 <g2199192503@gmail.com>
2026-03-02 13:01:35 +0900

    Deleted Branch "new"

--- new/PY/check/RIJ_17regions_duplicates.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("./CSV/gridset/RIJ_17regions.csv")
-
-# 找出 (I, J) 均重复的行
-duplicates = df[df.duplicated(subset=["I", "J"], keep=False)]
-
-duplicates.to_csv("./CSV/RIJ_duplicates.csv", index=False)
--- new/PY/tools/archive/cleanRIJ.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df= pd.read_csv("./CSV/RIJ.csv")
-
-df = df.drop(columns =["Text"])
-
-df.to_csv("./CSV/RIJ.csv",index =False)
